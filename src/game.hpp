d53f1362384bed16e35fbcce713f3de5eef071be
exucutional <exdevour@gmail.com>
2019-04-16 22:03:03 +0300

    save

--- src/game.hpp
+++ src/game.hpp
@@ -11,13 +11,10 @@ class App
 	Player player;
 	AssetManager aManager;
 public:
-<<<<<<< HEAD
 	Game() {}
 	void run();
-=======
 	int8_t run();
 	int8_t gameLoop();
->>>>>>> 2627b3ede6c71f435e838b902612e8d613497dd2
 };
 
 #endif //GAME_HPP_
